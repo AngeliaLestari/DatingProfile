59b219c82e4cb60a19d220e64e0b7853d2df7693
AngeliaLestari <72073971+AngeliaLestari@users.noreply.github.com>
2021-01-10 11:57:11 +0800

    Add files via upload

---
+++ Dating Profile/Dating Profile/Profile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dating_Profile
+{
+    class Profile
+    {
+        public string name;
+        public int age;
+        public string city;
+        public string country;
+        public string pronouns;
+        public string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string pronouns = "Ms/Mr")
+
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+
+        public string ViewProfile()
+        {
+            string ViewProfile = $"My name is {name},\n I am {age} years old,\n {pronouns} is coming from {city}, {country}";
+            string CompleteViewProfile = ViewProfile;
+
+            CompleteViewProfile += "\n Hobbies are";
+            foreach (string hobby in hobbies)
+                {
+                CompleteViewProfile += $"\n {hobby}";          
+                }
+
+            return CompleteViewProfile;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+    
+        
+    }
+}
---
+++ Dating Profile/Dating Profile/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dating_Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam", 35, "Tulungagung", "Swiss");
+            sam.SetHobbies(new string[] {"Tidur", "Makan Cabe", "Ngupil", "Berenang"});
+
+            Console.WriteLine(sam.ViewProfile());
+
+            Profile Mary = new Profile("Mary", 40, "Malang", "Belanda");
+            Mary.SetHobbies(new string[] { "Molor", "Membaca", "Menyanyi", "Bersepeda" });
+            Console.WriteLine(Mary.ViewProfile());
+
+        }
+                    
+           
+
+
+        
+    }
+}
